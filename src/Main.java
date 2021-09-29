153b810761a5150382086b7049fd4c11b8a719b6
Natalia Petyaeva <npetyaeva@gmail.com>
2021-09-29 10:57:02 +0300

    Modifed Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         int currentBalance = 2000000000;
-        int transferAmount = 500000;
+        int transferAmount = 500000000;
         int finalValue = currentBalance + transferAmount;
         System.out.println(finalValue);
     }
