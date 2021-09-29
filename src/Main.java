071c0727f9e9a882d44a8521f44643fe494d40d9
Natalia Petyaeva <npetyaeva@gmail.com>
2021-09-29 10:12:20 +0300

    Added Main.java

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int currentBalance = 2000000000;
+        int transferAmount = 500000;
+        int finalValue = currentBalance + transferAmount;
+        System.out.println(finalValue);
+    }
+}
